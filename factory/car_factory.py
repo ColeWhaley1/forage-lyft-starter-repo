a15839feb9f5edeb183bdfd8a78203d2e9e798b1
ColeWhaley1 <colewhaley1@gmail.com>
2024-08-30 14:07:24 -0400

    add both tire brands to car

--- factory/car_factory.py
+++ factory/car_factory.py
@@ -4,6 +4,8 @@ from car import Car
 from engine.capulet_engine import CapuletEngine
 from engine.sternman_engine import SternmanEngine
 from engine.willoughby_engine import WilloughbyEngine
+from tires.carrigans import Carrigans
+from tires.octoprimes import Octoprimes
 
 
 class CarFactory:
@@ -11,28 +13,33 @@ class CarFactory:
     def create_calliope(current_date, last_service_date, current_mileage, last_service_mileage) -> Car:
         engine = CapuletEngine(current_mileage, last_service_mileage)
         battery = SpindlerBattery(last_service_date, current_date)
-        return Car(engine, battery)
+        tires = Carrigans([0, 0, 0, 0])
+        return Car(engine, battery, tires)
     
     @staticmethod
     def create_glissade(current_date, last_service_date, current_mileage, last_service_mileage) -> Car:
         engine = WilloughbyEngine(current_mileage, last_service_mileage)
         battery = SpindlerBattery(last_service_date, current_date)
-        return Car(engine, battery)
+        tires = Carrigans([0, 0, 0, 0])
+        return Car(engine, battery, tires)
     
     @staticmethod
     def create_palindrome(current_date, last_service_date, warning_light_on) -> Car:
         engine = SternmanEngine(warning_light_on)
         battery = SpindlerBattery(last_service_date, current_date)
-        return Car(engine, battery)
+        tires = Carrigans([0, 0, 0, 0])
+        return Car(engine, battery, tires)
     
     @staticmethod
     def create_rorsach(current_date, last_service_date, current_mileage, last_service_mileage) -> Car:
         engine = WilloughbyEngine(current_mileage, last_service_mileage)
         battery = NubbinBattery(last_service_date, current_date)
-        return Car(engine, battery)
+        tires = Octoprimes([0, 0, 0, 0])
+        return Car(engine, battery, tires)
     
     @staticmethod
     def create_thovex(current_date, last_service_date, current_mileage, last_service_mileage) -> Car:
         engine = CapuletEngine(current_mileage, last_service_mileage)
         battery = NubbinBattery(last_service_date, current_date)
-        return Car(engine, battery)
+        tires = Octoprimes([0, 0, 0, 0])
+        return Car(engine, battery, tires)
--- test/test_car.py
+++ test/test_car.py
@@ -6,6 +6,8 @@ from battery.spindler_battery import SpindlerBattery
 from engine.capulet_engine import CapuletEngine
 from engine.sternman_engine import SternmanEngine
 from engine.willoughby_engine import WilloughbyEngine
+from tires.carrigans import Carrigans
+from tires.octoprimes import Octoprimes
 
 class TestCapulet(unittest.TestCase):
     def test_engine_should_be_serviced(self):
@@ -131,6 +133,44 @@ class TestNubbin(unittest.TestCase):
 
         battery = NubbinBattery(last_service_date, mock_current_date)
         self.assertFalse(battery.needs_service())
+
+class TestCarrigan(unittest.TestCase):
+    def test_tires_should_be_serviced(self):
+        wear = [0.9, 0, 0, 0]
+        tires = Carrigans(wear)
+
+        self.assertTrue(tires.needs_service())
+
+    def test_tires_should_be_serviced_more_than_one(self):
+        wear = [1, 1, 1, 1]
+        tires = Carrigans(wear)
+
+        self.assertTrue(tires.needs_service())
+
+    def test_tires_should_not_be_serviced(self):
+        wear = [0, 0, 0, 0]
+        tires = Carrigans(wear)
+
+        self.assertFalse(tires.needs_service())
+
+class TestOctoprime(unittest.TestCase):
+    def test_tires_should_be_serviced(self):
+        wear = [1, 1, 1, 1]
+        tires = Octoprimes(wear)
+
+        self.assertTrue(tires.needs_service())
+
+    def test_tires_should_be_serviced_meets_threshold(self):
+        wear = [1, 1, 1, 0]
+        tires = Octoprimes(wear)
+
+        self.assertTrue(tires.needs_service())
+
+    def test_tires_should_not_be_serviced(self):
+        wear = [0, 0, 0, 0]
+        tires = Octoprimes(wear)
+
+        self.assertFalse(tires.needs_service())
 
 if __name__ == '__main__':
     unittest.main()
